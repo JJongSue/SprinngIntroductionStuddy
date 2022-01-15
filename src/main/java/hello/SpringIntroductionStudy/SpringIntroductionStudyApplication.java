b3eeaa1a432d3936737a4b116bddfd08f7e7a5cc
jspark940321 <jspark940321@gmail.com>
2022-01-16 00:47:57 +0900

    inflearn kmh study start

---
+++ src/main/java/hello/SpringIntroductionStudy/SpringIntroductionStudyApplication.java
@@ -0,0 +1,13 @@
+package hello.SpringIntroductionStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductionStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntroductionStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/SpringIntroductionStudy/SpringIntroductionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.SpringIntroductionStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroductionStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
